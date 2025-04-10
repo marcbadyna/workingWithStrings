c6482a16189d3b0f46092ba9961569b9012d4d50
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-04-10 09:52:04 -0400

    Added framing to the greeting. Used size function to determine size of users name.

--- workingWithStrings/main.cpp
+++ workingWithStrings/main.cpp
@@ -9,15 +9,24 @@
 #include <string>
 
 int main() {
-    // ask for the person's name
+    
     std::cout<<"Please enter your first name: ";
+    std::string name;
+    std::cin>>name;
     
-    // read the name
-    std::string name;   // define name
-    std::cin>>name;     // read into name
+    const std:: string greeting = "Hello, " + name + "!";
     
-    // write a greeting
-    std::cout<<"Hello, "<<name<<"!"<<std::endl;
+    const std::string spaces(greeting.size(), ' ');
+    const std::string second = "* " + spaces + " *";
+    
+    const std::string first(second.size(), '*');
+    
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
     
     return 0;
 }
