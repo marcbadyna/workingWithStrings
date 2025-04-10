316afe7b7b31e7bd819a3d1a9734a59b9e05c24c
marcbadyna <150734285+marcbadyna@users.noreply.github.com>
2025-04-10 09:44:11 -0400

    Chapter 1 - Working with strings. Included the strings library. Asked for user to enter name. Output a greeting.

--- workingWithStrings/main.cpp
+++ workingWithStrings/main.cpp
@@ -6,9 +6,18 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    // ask for the person's name
+    std::cout<<"Please enter your first name: ";
+    
+    // read the name
+    std::string name;   // define name
+    std::cin>>name;     // read into name
+    
+    // write a greeting
+    std::cout<<"Hello, "<<name<<"!"<<std::endl;
+    
     return 0;
 }
